e2b7870fb45d13af77a7a7186fb9fcc2749e782e
Wenying Chang <changwenyingnl@gmail.com>
2024-12-09 21:41:32 +0100

    feat:/try colors and customization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,25 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#000000",
+        heatmap: {
+          "5": "#7F0000",
+          "4": "#A50026",
+          "3": "#D73027",
+          "2": "#F46D43",
+          "1": "#FD9A6D",
+          "0": "#FDDDBC",
+        }
       },
     },
   },
