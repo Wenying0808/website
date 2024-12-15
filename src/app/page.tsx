9f221e75fbeaee4580502a6ca8c1b4b20c5787eb
Wenying Chang <changwenyingnl@gmail.com>
2024-12-15 11:08:50 +0100

    try out typography class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,15 @@ export default function Home() {
       <p className="bg-[#012345] text-indigo-50">
         paragraph
       </p>
-      <h1 className="text-3xl">Title1</h1>
-      <h2 className="text-2xl">Title2</h2>
-      <h3 className="text-xl">Title3</h3>
-      <p className="text-xs">Paragraph text-xs</p>
-      <p className="text-lg">Paragraph text-lg</p>
+      <h1 className="text-3xl text-indigo-400underline">Title1</h1>
+      <h2 className="text-2xl text-indigo-400">Title2</h2>
+      <h3 className="text-xl text-indigo-400 uppercase underline decoration-wavy decoration-indigo-400 decoration-1 underline-offset-4 leading-normal">I am title3</h3>
+      <p className="text-xs text-indigo-400">Paragraph text-xs</p>
+      <p className="text-lg text-indigo-400 italic">Paragraph text-lg</p>
+      <p className="text-sm text-indigo-400 truncate">This is a long sentence with a lot of words to explain that truncate will truncate the text</p>
+      <div className="whitespace-normal">
+        <p className="text-indigo-400">This is a long sentence with a lot of words</p>
+      </div>
     </div>
   );
 }
