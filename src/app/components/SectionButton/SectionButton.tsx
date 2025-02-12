86ac298e9ced2ea29218ea42c206ed689a27096f
Wenying Chang <changwenyingnl@gmail.com>
2025-02-12 12:15:19 +0100

    fix:/precedence of mui button styles

--- src/app/components/SectionButton/SectionButton.tsx
+++ src/app/components/SectionButton/SectionButton.tsx
@@ -17,8 +17,8 @@ export const SectionButton = ({ label, url }: Props) => {
             onClick={handleClick} 
             variant="contained" 
             size="small" 
-            sx={{ textTransform: 'none' }} 
-            className="rounded-full px-4 bg-primary-light dark:bg-primary-dark  hover:scale-105 !important"
+            sx={{ textTransform: 'none', borderRadius: '9999px', padding: '4px 16px', backgroundColor:'#4758DC'}} 
+            className="rounded-full bg-primary-light px-4 dark:bg-primary-dark  hover:scale-105"
         >
             {label}
         </Button>
